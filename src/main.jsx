9fa45933a677a2c94c547a40353a4567709fa655
Javier Carrasco <javierdeveloper08@gmail.com>
2023-05-09 16:40:10 -0400

    feat(home): Creando la pagina Root

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
+import ContainerCard from "./components/container-card/ContainerCard.jsx";
+import Root from "./routes/Root.jsx";
 import "./index.scss";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <ContainerCard />,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
