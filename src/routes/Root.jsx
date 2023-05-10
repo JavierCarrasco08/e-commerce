84243f34460059adf62df28b87daeb89686dc284
Javier Carrasco <javierdeveloper08@gmail.com>
2023-05-10 16:15:01 -0400

    feat(useReducer): Usando el Hook reduce y refactorizando el codigo

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,49 +1,20 @@
-import { useState } from "react";
+import { productReduce } from "../reducer/productReduce";
+import { useReducer } from "react";
 import { AddContext } from "./../context/addCar";
 import { Outlet } from "react-router-dom";
 import BuyingMenu from "./../components/buying-menu/BuyingMenu";
 import CategoryMenu from "./../components/category-menu/CategoryMenu";
-import { getAll } from "./../helper/getAll";
 
 function Root() {
-  const [buyList, setBuyList] = useState([]);
+  const [buyList, dispatch] = useReducer(productReduce, []);
   return (
     <>
       <AddContext.Provider
         value={(id, op) => {
-          let ignore = true;
-          let getProduct = getAll().find((product) => product.id === id);
-          let filterProduct = buyList.find((product) => product.id === id);
-          getProduct.num = 1;
-          if (!buyList.some((product) => product.id === id)) {
-            setBuyList([...buyList, getProduct]);
-            ignore = false;
-          }
-          if (filterProduct) {
-            let result = op(filterProduct.num);
-            result === 0
-              ? setBuyList(
-                  buyList.filter((product) => {
-                    ignore = false;
-                    return product.id !== id;
-                  })
-                )
-              : (ignore = true);
-          }
-          if (ignore) {
-            setBuyList(
-              buyList.map((product) => {
-                if (product.id === id) {
-                  return {
-                    ...product,
-                    num: op(product.num),
-                  };
-                } else {
-                  return product;
-                }
-              })
-            );
-          }
+          let type = buyList.find((product) => product.id === id)
+            ? "increment"
+            : "add";
+          dispatch({ type, id, op });
         }}>
         <CategoryMenu />
         <Outlet />
