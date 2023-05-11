c38d21e9dbfbc163c5c7839205a6ec325204e15d
Javier Carrasco <javierdeveloper08@gmail.com>
2023-05-11 08:29:22 -0400

    fix: Usando el componente prices, y ajustando estilos

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,27 +1,18 @@
-import { useContext } from "react";
-import { AddContext } from "../../context/addCar";
+import { Link } from "react-router-dom";
+import Prices from "../prices-add/Prices";
 import "./card.scss";
 
 export default function Card({ name, prices, src, id }) {
-  const Add = useContext(AddContext);
   return (
     <div className="card">
       <figure className="card__figure">
         <img src={src} alt={name} className="card__img" />
       </figure>
       <h2 className="card__name">{name}</h2>
-      <div className="card__content">
-        <p className="card__price">
-          {prices} <span>$</span>
-        </p>
-        <button
-          className="card__button"
-          onPointerDown={() => {
-            Add(id, (num) => num + 1);
-          }}>
-          Add
-        </button>
-      </div>
+      <Link to={`/item/${id}`} className="card__link">
+        See item
+      </Link>
+      <Prices id={id} prices={prices} />
     </div>
   );
 }
